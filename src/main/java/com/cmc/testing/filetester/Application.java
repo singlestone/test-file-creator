d92feedbef0c4e1e3d4861567735189d51dc073b
Jay Hogan <seeker136@gmail.com>
2016-02-05 13:20:58 -0500

    Break the build

--- src/main/java/com/cmc/testing/filetester/Application.java
+++ src/main/java/com/cmc/testing/filetester/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        //Break the build
+        Break the build
         SpringApplication.run(Application.class, args);
     }
 }
